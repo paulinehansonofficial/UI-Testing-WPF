65b48bdee81866896e162dde75cd0988176ed904
paulinehansonofficial <mich420n0sk0pe@gmail.com>
2018-12-05 22:37:02 +1100

    Initial

--- SimpleWPFApplication/MainWindow.xaml.cs
+++ SimpleWPFApplication/MainWindow.xaml.cs
@@ -18,11 +18,34 @@ namespace SimpleWPFApplication
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public delegate void ProgressBarDelegate(System.Windows.DependencyProperty dp, Object value);
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void best_button_Click(object sender, RoutedEventArgs e)
+        {
+            double progress = 0;
+
+            ProgressBarDelegate updatePbDelegate =
+                new ProgressBarDelegate(prog_bar.SetValue);
+
+            do
+            {
+                progress++;
+
+                Dispatcher.Invoke(updatePbDelegate,
+                    System.Windows.Threading.DispatcherPriority.Background,
+                    new object[] { ProgressBar.ValueProperty, progress });
+                prog_bar.Value = progress;
+            }
+            while (prog_bar.Value != prog_bar.Maximum);
+
+            check_box.IsEnabled = true;
         }
     }
 }
